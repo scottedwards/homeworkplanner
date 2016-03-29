8d3b5a1fcaee8a334ae22da73a10a3c27db8d187
Scott Edwards <scottedwardsicoll@gmail.com>
2016-03-29 12:40:14 +0100

    Sorting Completed!
    Sorting now works for both tables

--- js/sort-table.js
+++ js/sort-table.js
@@ -1,9 +1,28 @@
+function addRowToTree(row, arg, sortingTree) {
+	//get the id for each homework so we can identify each title/date after sorting
+		//i shall input the sorting data along with its associating ID
+		var homeworkID = row.find("h3").attr("id");
+		if (arg == "title") {
+			//get the title for the homework
+			var title = row.find("h3").text();
+			//add the title and id to the binary tree as a "tuple" but really its just an array 
+			sortingTree.addNode([homeworkID, title]);
+		} else if(arg == "date") {
+			//get the due date for the homework
+			var dueDate = row.find("td").attr("dueDate");
+			//add the due date and the id to the binary tree
+			sortingTree.addNode([homeworkID, dueDate]);
+		}
+}
+
 function sortBy(arg) {
 	console.log("Sorting by " + arg + "...");
 	var sortingTree = new BinaryTree();
 
 	//sort due-in table
-	$("#due-in > tbody > tr").each(addRowToTree($(this), arg, sortingTree));
+	$("#due-in > tbody > tr").each(function() {
+		addRowToTree($(this), arg, sortingTree);
+	});
 	var sortedList = sortingTree.getOrderedValues();
 	
 	for (var i = 1; i <= sortedList.length - 1; i++) {
@@ -15,7 +34,9 @@ function sortBy(arg) {
 
 	//sort passed table
 	sortingTree = new BinaryTree();
-	$("#passed-table > tbody > tr").each(addRowToTree($(this), arg, sortingTree));
+	$("#passed-table > tbody > tr").each(function() {
+		addRowToTree($(this), arg, sortingTree);
+	});
 	var sortedList = sortingTree.getOrderedValues();
 	
 	for (var i = 1; i <= sortedList.length - 1; i++) {
@@ -48,19 +69,3 @@ function sortBy(arg) {
 	
 }
 
-function addRowToTree(row, arg, sortingTree) {
-	//get the id for each homework so we can identify each title/date after sorting
-		//i shall input the sorting data along with its associating ID
-		var homeworkID = row.find("h3").attr("id");
-		if (arg == "title") {
-			//get the title for the homework
-			var title = row.find("h3").text();
-			//add the title and id to the binary tree as a "tuple" but really its just an array 
-			sortingTree.addNode([homeworkID, title]);
-		} else if(arg == "date") {
-			//get the due date for the homework
-			var dueDate = row.find("td").attr("dueDate");
-			//add the due date and the id to the binary tree
-			sortingTree.addNode([homeworkID, dueDate]);
-		}
-}
